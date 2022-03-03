2f4b80f3a51481b665ec7e0edd38dbd3794b582f
MelvinPereira <pereiramelvin78@gmail.com>
2022-03-02 23:55:23 -0600

    mensaje final

--- app/src/main/java/com/example/pizzeria_juanito/PedidoFinal.java
+++ app/src/main/java/com/example/pizzeria_juanito/PedidoFinal.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
@@ -35,10 +36,15 @@ public class PedidoFinal extends AppCompatActivity {
         aceptar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),MainActivity.class);
-                startActivity(i);
-                Toast.makeText(getApplicationContext(),"Pedido Realizado Con exito",Toast.LENGTH_LONG);
-                finish();
+                Toast.makeText(getApplicationContext(),"Pedido Realizado Con exito",Toast.LENGTH_LONG).show();
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                        startActivity(i);
+                        finish();
+                    }
+                },4000);
 
             }
         });
